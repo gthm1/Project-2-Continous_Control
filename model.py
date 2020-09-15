297bb159ee8705e88b499ca61128a150abf4cd3d
gthm1 <gthm298@gmail.com>
2020-09-15 14:12:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -10,15 +10,14 @@ class Actor(nn.Module):
         self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 =  nn.Linear(fc2_units, action_size)
-        self.reset_parameters()                
-    def forward(self, state):
-        x = F.relu(self.bn1(self.fc1(state)))
-        x = F.relu(self.fc2(x))
-        return torch.tanh(self.fc3(x)) 
+        self.reset_parameters() 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+    def forward(self, state):
+        x = F.relu(self.fc2(F.relu(self.bn1(self.fc1(state)))))
+        return torch.tanh(self.fc3(x))     
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
     lim = 1. / np.sqrt(fan_in)
@@ -34,8 +33,7 @@ class Critic(nn.Module):
         self.reset_parameters()      
     def forward(self, state, action):
         x = F.relu(self.bn1(self.fc1(state)))
-        x = torch.cat((x, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc2(torch.cat((x, action), dim=1)))
         return self.fc3(x)
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
